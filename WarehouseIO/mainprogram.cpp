da09f579a2f70aff0b8a899916d0e64b50234566
SanTransisco <Santransisco@yahoo.com>
2017-03-11 14:44:57 -0800

    Add files via upload

--- WarehouseIO/mainprogram.cpp
+++ WarehouseIO/mainprogram.cpp
@@ -19,62 +19,52 @@ void MainProgram::on_pushButton_3_clicked()
                                                         tr("Open Member Info File"),
                                                         "C://",
                                                         "All files(*x*);;Text File(*.txt)");
-    try{
-        database.LoadMember(fileDirectory);
-    }
-    catch(string& e)
-    {
-        QString error = error.fromStdString(e);
-        ui->MemberListError->setText(error);
-    }
+    database.LoadMember(fileDirectory);
 
+    ui->MemberListError->setText(QString::number(database.memberListSize()));
 }
+//void MainProgram::LoadMemberTable()
+//{
+//    QLabel* temp = new QLabel();
+
+//    ui->MemberDataTable;
+//}
+
 void MainProgram::on_GenerateReport_clicked()
 {
+    QString fileDirectory = QFileDialog::getSaveFileName(this,
+                                                         tr("name the file"),
+                                                         "C://",
+                                                         "Text File(*.txt)");
+    QFile closeFile(fileDirectory);
     Date salesReportdate;
     salesReportdate.Set(ui->DateInput->text());
 }
-// Returns member from memberList vector at given index
-const Member MainProgram::GetMember(QVector<Member*> memberList, int index)
+
+void MainProgram::on_Filebrowser_clicked()
 {
-    return *memberList[index];
+    QString fileDirectory = QFileDialog::getOpenFileName(this,
+                                                         tr("Open Member Info File"),
+                                                         "C://",
+                                                         "All files(*x*)::Text File(*.txt)");
+    ui->FileDiectoryReportSales->setText(fileDirectory);
 }
-// Returns itemStruct from itemList vector at given index
-const itemStruct MainProgram::GetItem(QVector<itemStruct> itemList, int index)
+
+void MainProgram::on_ReportFileContents_clicked()
 {
-    return itemList[index];
+     database.loadSalesReport(ui->FileDiectoryReportSales->text());
 }
-// Returns TransactionNode from transactionList vector at given index
-const TransactionNode MainProgram::GetTransaction(QVector<TransactionNode> TransactionList, int index)
+
+void MainProgram::on_Exit_clicked()
 {
-    return TransactionList[index];
+
 }
-void MainProgram::OutputToMemberTable(QVector<Member*> memberList)
+
+void MainProgram::on_SaveButton_clicked()
 {
-    QTableWidgetItem* item;
-    for (int col = 0; col < MEMBER_TABLE_COL_SIZE; col++)
-    {
-        for (int row = 0; row < memberList.size(); row++)
-        {
-            switch(col)
-            {
-                         // Creates and outputs QTableWidgetItem Name of member
-                case 0 : item = new QTableWidgetItem(memberList[row]->getName());
-                    break;
-                         // Creates and outputs QTableWidgetItem ID of member
-                case 1 : item = new QTableWidgetItem(memberList[row]->getId());
-                    break;
-                         // Creates and outputs QTableWidgetItem Membership Type of member
-                case 2 : item = new QTableWidgetItem(memberList[row]->getMembershipQString());
-                    break;
-                         // Creates and outputs QTableWidgetItem Rebate of member
-                case 3 : item = new QTableWidgetItem(memberList[row]->getRebate());
-                    break;
-                         // Creates and outputs QTableWidgetItem Expiration Date of member
-                case 4 : item = new QTableWidgetItem(memberList[row]->getDate());
-                    break;
-            }
-            ui->MemberTable->setItem(row, col, item);
-        }
-    }
+    QString fileDirectory = QFileDialog::getOpenFileName(this,
+                                                        tr("Save to File:"),
+                                                        "C://",
+                                                        "All files(*x*)::Text File(*.txt)");
+    database.SaveMembers(fileDirectory);
 }
