6165e6ebac9fc5d33d448a2da8a5a58f037849c3
25wes25 <wesleyswanson97@gmail.com>
2017-03-11 14:51:28 -0800

    Add files via upload

--- WarehouseIO/mainprogram.cpp
+++ WarehouseIO/mainprogram.cpp
@@ -19,52 +19,62 @@ void MainProgram::on_pushButton_3_clicked()
                                                         tr("Open Member Info File"),
                                                         "C://",
                                                         "All files(*x*);;Text File(*.txt)");
-    database.LoadMember(fileDirectory);
+    try{
+        database.LoadMember(fileDirectory);
+    }
+    catch(string& e)
+    {
+        QString error = error.fromStdString(e);
+        ui->MemberListError->setText(error);
+    }
 
-    ui->MemberListError->setText(QString::number(database.memberListSize()));
 }
-//void MainProgram::LoadMemberTable()
-//{
-//    QLabel* temp = new QLabel();
-
-//    ui->MemberDataTable;
-//}
-
 void MainProgram::on_GenerateReport_clicked()
 {
-    QString fileDirectory = QFileDialog::getSaveFileName(this,
-                                                         tr("name the file"),
-                                                         "C://",
-                                                         "Text File(*.txt)");
-    QFile closeFile(fileDirectory);
     Date salesReportdate;
     salesReportdate.Set(ui->DateInput->text());
 }
-
-void MainProgram::on_Filebrowser_clicked()
+// Returns member from memberList vector at given index
+const Member MainProgram::GetMember(QVector<Member*> memberList, int index)
 {
-    QString fileDirectory = QFileDialog::getOpenFileName(this,
-                                                         tr("Open Member Info File"),
-                                                         "C://",
-                                                         "All files(*x*)::Text File(*.txt)");
-    ui->FileDiectoryReportSales->setText(fileDirectory);
+    return *memberList[index];
 }
-
-void MainProgram::on_ReportFileContents_clicked()
+// Returns itemStruct from itemList vector at given index
+const itemStruct MainProgram::GetItem(QVector<itemStruct> itemList, int index)
 {
-     database.loadSalesReport(ui->FileDiectoryReportSales->text());
+    return itemList[index];
 }
-
-void MainProgram::on_Exit_clicked()
+// Returns TransactionNode from transactionList vector at given index
+const TransactionNode MainProgram::GetTransaction(QVector<TransactionNode> TransactionList, int index)
 {
-
+    return TransactionList[index];
 }
-
-void MainProgram::on_SaveButton_clicked()
+void MainProgram::OutputToMemberTable(QVector<Member*> memberList)
 {
-    QString fileDirectory = QFileDialog::getOpenFileName(this,
-                                                        tr("Save to File:"),
-                                                        "C://",
-                                                        "All files(*x*)::Text File(*.txt)");
-    database.SaveMembers(fileDirectory);
+    QTableWidgetItem* item;
+    for (unsigned col = 0; col < MEMBER_TABLE_COL_SIZE; col++)
+    {
+        for (unsigned row = 0; row < memberList.size(); row++)
+        {
+            switch(col)
+            {
+                         // Creates and outputs QTableWidgetItem Name of member
+                case 0 : item = new QTableWidgetItem(memberList[row]->getName());
+                    break;
+                         // Creates and outputs QTableWidgetItem ID of member
+                case 1 : item = new QTableWidgetItem(memberList[row]->getId());
+                    break;
+                         // Creates and outputs QTableWidgetItem Membership Type of member
+                case 2 : item = new QTableWidgetItem(memberList[row]->getMembershipQString());
+                    break;
+                         // Creates and outputs QTableWidgetItem Rebate of member
+                case 3 : item = new QTableWidgetItem(memberList[row]->getRebate());
+                    break;
+                         // Creates and outputs QTableWidgetItem Expiration Date of member
+                case 4 : item = new QTableWidgetItem(memberList[row]->getDate());
+                    break;
+            }
+            ui->MemberTable->setItem(row, col, item);
+        }
+    }
 }
